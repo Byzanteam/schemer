5ac9560dbb657ca34cdf1dbf1b657cb3af5ff29a
Phil Chen <06fahchen@gmail.com>
2022-12-29 14:58:59 +0800

    chore: add ex_doc

--- mix.exs
+++ mix.exs
@@ -28,7 +28,8 @@ defmodule Schemer.MixProject do
     [
       {:typed_struct, "~> 0.3.0"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
