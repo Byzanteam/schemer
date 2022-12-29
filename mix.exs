3682c13eca19144b31b52d455cf5301be89657f9
Phil Chen <06fahchen@gmail.com>
2022-12-29 12:01:59 +0800

    ci: init

--- mix.exs
+++ mix.exs
@@ -15,33 +15,32 @@ defmodule Schemer.MixProject do
         name: "schemer",
         licenses: ["MIT"],
         files: ~w(lib mix.exs mix.lock .tool-versions README.md),
-        links: %{
-          "GitHub" => "https://github.com/Byzanteam/schemer"
-        }
+        links: %{"GitHub" => "https://github.com/Byzanteam/schemer"}
       ]
     ]
   end
 
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    [extra_applications: [:logger]]
   end
 
   defp deps do
     [
       {:typed_struct, "~> 0.3.0"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.1", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 
   defp aliases do
-    [
-      "code.check": ["format --check-formatted", "credo --strict", "dialyzer"]
-    ]
+    ["code.check": ["format --check-formatted", "credo --strict", "dialyzer"]]
   end
 
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_), do: ["lib"]
+  defp elixirc_paths(:test) do
+    ["lib", "test/support"]
+  end
+
+  defp elixirc_paths(_) do
+    ["lib"]
+  end
 end
